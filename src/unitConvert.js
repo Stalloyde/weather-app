8a9dcf0cf613f15c700ec7b5616f29bf1be1c11a
Stalloyde <stalloyde93@gmail.com>
2023-02-06 13:44:58 +0800

    Fix wrong calculations

--- src/unitConvert.js
+++ src/unitConvert.js
@@ -1,11 +1,11 @@
 function fahrenheitToCelsius(fahrenheit) {
-  const celsius = ((fahrenheit - 32) * 5) / 9;
+  const celsius = (fahrenheit - 32) * 0.5556;
   const celsiusRounded = Math.round(celsius * 10) / 10;
   return celsiusRounded;
 }
 
 function celsiusToFahrenheit(celsius) {
-  const fahrenheit = (celsius * 9) / 5 + 32;
+  const fahrenheit = celsius * 1.8 + 32;
   const fahrenheitRounded = Math.round(fahrenheit * 10) / 10;
   return fahrenheitRounded;
 }
